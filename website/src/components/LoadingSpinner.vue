a7ef6e45776f8690f87cec287bcab6689486091b
tbarreau-2084 <tbarreau@2084gallery.com>
2021-05-11 21:52:03 +0200

    CU-jk6vuw[review] Replace spinner javascript with scss mixin

--- website/src/components/LoadingSpinner.vue
+++ website/src/components/LoadingSpinner.vue
@@ -1,15 +1,9 @@
 <template>
   <div id="loading-spinner" v-show="loading">
-    <transition-group name="slide" tag="span">
-      <div
-      v-for="element in spinnerElements"
-      :key="element.content"
-      class="absolute"
-      :style="{ top: element.position.top, left: element.position.left, transition: 'all 2s ease', color: color}"
-      >
-        {{element.content}}
-      </div>
-    </transition-group>
+    <div class="top-left absolute">2</div>
+    <div class="top-right absolute">0</div>
+    <div class="bottom-right absolute">8</div>
+    <div class="bottom-left absolute">4</div>
   </div>
 </template>
 <script>
@@ -19,64 +13,8 @@ export default {
     loading: {
       type: Boolean
     }
-  },
-  data: () => ({
-    counter: 0,
-    color: '#000',
-    oldEl: {},
-    renderComponent: true,
-    spinnerElements: []
-  }),
-  methods: {
-    settingElementsPosition () {
-      this.color = '#999999'
-      for (; this.counter < this.spinnerElements.length; this.counter++) {
-        if (this.counter === 3) {
-          this.spinnerElements[this.counter].position = this.oldEl.position
-          this.spinnerElements[this.counter].to = this.oldEl.to
-          return
-        }
-        this.spinnerElements[this.counter].position = this.spinnerElements[this.counter + 1].position
-        this.spinnerElements[this.counter].to = this.spinnerElements[this.counter + 1].to
-      }
-    },
-    initiatingSpin (timer) {
-      this.oldEl.position = this.spinnerElements[0].position
-      this.oldEl.to = this.spinnerElements[0].to
-      this.counter = 0
-      setTimeout(() => (
-        this.settingElementsPosition()
-      ), timer)
-    }
-  },
-  mounted () {
-    const leftPositions = (!this.$q.platform.is.desktop) ? ['35%', '55%'] : ['45%', '50%']
-    this.spinnerElements.push(
-      { content: '2', position: { top: '40%', left: leftPositions[0] }, to: 'right' },
-      { content: '0', position: { top: '40%', left: leftPositions[1] }, to: 'down' },
-      { content: '8', position: { top: '50%', left: leftPositions[1] }, to: 'left' },
-      { content: '4', position: { top: '50%', left: leftPositions[0] }, to: 'up' }
-    )
-    this.initiatingSpin(1000)
-  },
-  updated () {
-    this.initiatingSpin(2000)
   }
 }
 </script>
 <style lang="scss" scoped>
-#loading-spinner {
-  z-index: 999;
-  background-color: white;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  height: 100%;
-  width: 100%;
-  font-size: 4rem;
-  font-weight: 900;
-  color: #000;
-}
-
 </style>
