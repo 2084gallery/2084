92741edf109235a8ade0db06d7f67a0f24270463
tbarreau-2084 <tbarreau@2084gallery.com>
2021-05-13 15:37:14 +0200

    CU-gn3j5k[in progress] 🚀 Update package.json

--- website/server.js
+++ website/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const
   express = require('express'),
   serveStatic = require('serve-static'),
@@ -5,7 +6,6 @@ const
   port = process.env.PORT || 5000
 
 const app = express()
-
 app.use(history())
-app.use(serveStatic(__dirname))
+app.use(serveStatic(path.resolve(__dirname, '/dist/spa')))
 app.listen(port)
